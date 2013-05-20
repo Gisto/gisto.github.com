750efeeb0a6efdd254df4ba886dd3cb6edecd2fd
Sasha Khamkov <sanusart@gmail.com>
2013-05-20 23:09:47 +0300

    Add code to determine OS and provide link respectively (commented out in main.js until alpha is out)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-$(function() {
+$(function () {
 
-    $('.innsite').click(function(e) {
+    $('.innsite').click(function (e) {
         e.preventDefault();
         $.scrollTo(e.currentTarget.hash, 800);
     });
+
+/* Determine OS and provide ling respectively *\/
+
+    var platform = window.navigator.platform,
+        mainText = 'We auto determined that you need a {{OS}} installer',
+        linux = /Linux/g,
+        windows = /Win/g,
+        mac = /Mac/g;
+
+    if (linux.test(platform)) {
+        downloadLink('Linux', '#');
+    } else if (mac.test(platform)) {
+        downloadLink('Mac', '#');
+    } else if (windows.test(platform)) {
+        downloadLink('Windows', '#');
+    } else {
+        $('.download').html('<a class="btn links" href="">View download options on GitHub</a>');
+    }
+
+    function downloadLink(os, link) {
+        $('.download').html(mainText.replace('{{OS}}', os) + ' ' + '<a class="btn links" href="' + link + '"><i class="icon-download-alt"></i> Download for <br />' + os + ' </a>');
+    };
+/**/
 
 });
