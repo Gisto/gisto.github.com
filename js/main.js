0744041c72ddcee3371f0d07ac58d6d5cf8fe4db
Maayan Glikser <maayan@glikm.com>
2013-06-06 02:12:33 +0300

    Alpha version released. Updated links and builds

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ $(function () {
         $.scrollTo(e.currentTarget.hash, 800);
     });
 
-/* Determine OS and provide ling respectively *\/
+/* Determine OS and provide ling respectively */
 
     var platform = window.navigator.platform,
-        mainText = 'We auto determined that you need a {{OS}} installer',
+        mainText = ' <a class="download innsite" href="#download">Not running {{OS}}? Click Here</a>',
         linux = /Linux/g,
         windows = /Win/g,
         mac = /Mac/g;
 
     if (linux.test(platform)) {
-        downloadLink('Linux', '#');
+        downloadLink('Linux 64bit', 'download/0.1.0a/Gisto-0.1.0a-Linux_x86_64.tar.gz');
     } else if (mac.test(platform)) {
-        downloadLink('Mac', '#');
+        downloadLink('OSX 10.7+', 'download/0.1.0a/Gisto-0.1.0a-OSX_x86.dmg');
     } else if (windows.test(platform)) {
-        downloadLink('Windows', '#');
+        downloadLink('Windows', 'download/0.1.0a/Gisto-0.1.0a-Win_x86.exe');
     } else {
         $('.download').html('<a class="btn links" href="">View download options on GitHub</a>');
     }
@@ -26,6 +26,5 @@ $(function () {
     function downloadLink(os, link) {
         $('.download').html(mainText.replace('{{OS}}', os) + ' ' + '<a class="btn links" href="' + link + '"><i class="icon-download-alt"></i> Download for <br />' + os + ' </a>');
     };
-/**/
 
 });
